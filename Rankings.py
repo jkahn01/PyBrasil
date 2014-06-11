a33efc8e0d9136a43bdf3faecb892ee5df966248
Jason Kahn <jason.kahn@gmail.com>
2014-06-10 22:39:22 -0400

    Doug’s picks

--- Rankings.py
+++ Rankings.py
@@ -4,7 +4,7 @@ k_TEAMS = 32
 
 rankings = {
 	'Jay':{'Greece':15,'Russia':22,'Netherlands':12,'Germany':5,'Portugal':7,'Spain':2,'Italy':10,'Croatia':19,'France':16,'England':9,'Switzerland':13,'Belgium':4,'Bosnia-Herzegovina':23,'Algeria':25,"Cote d'Ivoire":24,'Ghana':17,'Cameroon':30,'Nigeria':29,'Mexico':18,'United States':14,'Honduras':20,'Costa Rica':26,'Argentina':3,'Brazil':1,'Chile':11,'Uruguay':6,'Colombia':8,'Ecuador':28,'Australia':32,'Japan':27,'South Korea':21,'Iran':31},
-	'Nick':{'Greece':1,'Russia':2,'Netherlands':3,'Germany':4,'Portugal':5,'Spain':6,'Italy':7,'Croatia':8,'France':9,'England':10,'Switzerland':11,'Belgium':12,'Bosnia-Herzegovina':13,'Algeria':14,"Cote d'Ivoire":15,'Ghana':16,'Cameroon':17,'Nigeria':18,'Mexico':19,'United States':20,'Honduras':21,'Costa Rica':22,'Argentina':23,'Brazil':24,'Chile':25,'Uruguay':26,'Colombia':27,'Ecuador':28,'Australia':29,'Japan':30,'South Korea':31,'Iran':32}
+	'Doug':{'Algeria':21,'Argentina':5,'Australia':32,'Belgium':11,'Bosnia-Herzegovina':18,'Brazil':2,'Cameroon':28,'Chile':14,'Colombia':7,'Costa Rica':23,"Cote d'Ivoire":25,'Croatia':19,'Ecuador':24,'England':10,'France':17,'Germany':4,'Ghana':26,'Greece':12,'Honduras':22,'Iran':31,'Italy':8,'Japan':27,'Mexico':29,'Netherlands':16,'Nigeria':15,'Portugal':30,'Russia':3,'South Korea':20,'Spain':1,'Switzerland':9,'United States':13,'Uruguay':6}
 }
 
 if __name__ == '__main__':
