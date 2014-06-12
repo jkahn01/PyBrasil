2cc8ef948e8576a67cfff57d496e84620901e285
Alex Alexiou <alex2iou@yahoo.com>
2014-06-12 16:59:01 -0400

    Alex ranks

--- Rankings.py
+++ Rankings.py
@@ -5,6 +5,7 @@ k_TEAMS = 32
 rankings = {
 	'Jay':{'Greece':15,'Russia':22,'Netherlands':12,'Germany':5,'Portugal':7,'Spain':2,'Italy':10,'Croatia':19,'France':16,'England':9,'Switzerland':13,'Belgium':4,'Bosnia-Herzegovina':23,'Algeria':25,"Cote d'Ivoire":24,'Ghana':17,'Cameroon':30,'Nigeria':29,'Mexico':18,'United States':14,'Honduras':20,'Costa Rica':26,'Argentina':3,'Brazil':1,'Chile':11,'Uruguay':6,'Colombia':8,'Ecuador':28,'Australia':32,'Japan':27,'South Korea':21,'Iran':31},
 	'Nick':{'Greece':1,'Russia':2,'Netherlands':3,'Germany':4,'Portugal':5,'Spain':6,'Italy':7,'Croatia':8,'France':9,'England':10,'Switzerland':11,'Belgium':12,'Bosnia-Herzegovina':13,'Algeria':14,"Cote d'Ivoire":15,'Ghana':16,'Cameroon':17,'Nigeria':18,'Mexico':19,'United States':20,'Honduras':21,'Costa Rica':22,'Argentina':23,'Brazil':24,'Chile':25,'Uruguay':26,'Colombia':27,'Ecuador':28,'Australia':29,'Japan':30,'South Korea':31,'Iran':32}
+	'Alex':{'Portugal':1,'Brazil':2,'England':3,'Belgium':4,'Colombia':5,'Bosnia-Herzegovina':17,'Spain':7,'France':8,'Croatia':9,'Argentina':10,'Ecuador':11,'Chile':24,'Uruguay':13,'United States':14,'South Korea':15,'Japan':16,'Germany':6,'Italy':18,'Ivory Coast':19,'Cameroon':20,'Honduras':21,'Ghana':22,'Algeria':23,'Nigeria':12,'Netherlands':25,'Russia':26,'Mexico':27,'Costa Rica':28,'Australia':29,'Switzerland':30,'Greece':31,'Iran':32}
 }
 
 if __name__ == '__main__':
